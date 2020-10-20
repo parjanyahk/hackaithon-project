8f78ce4bf35cce6ff75fdd571410e0996385c7e7
nehabalaji <nehabalaji10@gmail.com>
2020-10-20 14:47:52 +0530

    initial commit

---
+++ Android/app/src/main/java/com/example/hack_ai_thon_android/LogInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hack_ai_thon_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
---
+++ Android/app/src/main/java/com/example/hack_ai_thon_android/SurveyFormActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hack_ai_thon_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SurveyFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey_form)
+    }
+}
