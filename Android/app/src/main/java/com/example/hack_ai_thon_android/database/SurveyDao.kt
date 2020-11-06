90047e935990040568a4c7277ef5f3cdb8b95848
nehabalaji <nehabalaji10@gmail.com>
2020-11-06 11:15:32 +0530

    add entity class and dao

---
+++ Android/app/src/main/java/com/example/hack_ai_thon_android/database/SurveyDao.kt
@@ -0,0 +1,20 @@
+package com.example.hack_ai_thon_android.database
+
+import androidx.room.*
+import com.example.hack_ai_thon_android.data.SurveyData
+
+@Dao
+interface SurveyDao {
+
+    @Insert
+    fun insertSurvey(surveyData: SurveyData)
+
+    @Update
+    fun updateSurvey(surveyData: SurveyData)
+
+    @Delete
+    fun deleteSurvey(surveyData: SurveyData)
+
+    @Query("SELECT * FROM Survey")
+    fun getSurveyData()
+}
